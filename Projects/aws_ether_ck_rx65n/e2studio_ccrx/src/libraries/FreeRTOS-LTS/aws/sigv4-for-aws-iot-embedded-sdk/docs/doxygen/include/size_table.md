9b2a988a0c5f46dbcfb5890bf17526a126730e0f
Kazuki Mochizuki <kazuki.mochizuki.xk@renesas.com>
2022-10-28 19:32:22 +0900

    [update] Changed OTA demo to work with latest coreMQTT-Agent and coreMQTT and reduced ROM/RAM size
    Signed-off-by: Kazuki Mochizuki <kazuki.mochizuki.xk@renesas.com>

---
+++ Projects/aws_ether_ck_rx65n/e2studio_ccrx/src/libraries/FreeRTOS-LTS/aws/sigv4-for-aws-iot-embedded-sdk/docs/doxygen/include/size_table.md
@@ -0,0 +1,25 @@
+<table>
+    <tr>
+        <td colspan="3"><center><b>Code Size of AWS SigV4 library (example generated with GCC for ARM Cortex-M)</b></center></td>
+    </tr>
+    <tr>
+        <td><b>File</b></td>
+        <td><b><center>With -O1 Optimization</center></b></td>
+        <td><b><center>With -Os Optimization</center></b></td>
+    </tr>
+    <tr>
+        <td>sigv4.c</td>
+        <td><center>5.2K</center></td>
+        <td><center>4.4K</center></td>
+    </tr>
+    <tr>
+        <td>sigv4_quicksort.c</td>
+        <td><center>0.4K</center></td>
+        <td><center>0.3K</center></td>
+    </tr>
+    <tr>
+        <td><b>Total estimates</b></td>
+        <td><b><center>5.6K</center></b></td>
+        <td><b><center>4.7K</center></b></td>
+    </tr>
+</table>
---
+++ Projects/aws_ether_ck_rx65n/e2studio_ccrx/src/main.c
@@ -0,0 +1,223 @@
+/*
+FreeRTOS
+Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ http://aws.amazon.com/freertos
+ http://www.FreeRTOS.org
+*/
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Logging includes. */
+#include "iot_logging_task.h"
+
+/* Key provisioning includes. */
+#include "aws_dev_mode_key_provisioning.h"
+
+/* FreeRTOS+TCP includes. */
+#include "FreeRTOS_IP.h"
+
+/* Demo includes */
+#include "aws_demo.h"
+#include "aws_clientcredential.h"
+
+//#include "mqtt_agent_task.h"
+extern void UserInitialization(void);
+/**
+ * @brief Flag which enables OTA update task in background along with other demo tasks.
+ * OTA update task polls regularly for firmware update jobs or acts on a new firmware update
+ * available notification from OTA service.
+ */
+#define appmainINCLUDE_OTA_UPDATE_TASK            ( 1 )
+
+/**
+ * @brief Flag to enable or disable provisioning mode for the demo.
+ * Enabling the flags starts a CLI task, so that user can perform provisioning of the device through
+ * a serial terminal. Provisioning involves running commands to fetch or set the PKI and configuration
+ * information for the device to connect to broker and perform OTA updates. Disabling the flag results
+ * in disabling the CLI task and execution of the demo tasks in normal device operation mode.
+ */
+#define appmainPROVISIONING_MODE                  ( 1 )
+
+/**
+ * @brief Subscribe Publish demo tasks configuration.
+ * Subscribe publish demo task shows the basic functionality of connecting to an MQTT broker, subscribing
+ * to a topic, publishing messages to a topic and reporting the incoming messages on subscribed topic.
+ * Number of subscribe publish demo tasks to be spawned is configurable.
+ */
+#define appmainMQTT_NUM_PUBSUB_TASKS              ( 2 )
+#define appmainMQTT_PUBSUB_TASK_STACK_SIZE        ( 2048 )
+#define appmainMQTT_PUBSUB_TASK_PRIORITY          ( tskIDLE_PRIORITY +1 )
+
+/**
+ * @brief Stack size and priority for OTA Update task.
+ */
+#define appmainMQTT_OTA_UPDATE_TASK_STACK_SIZE    ( 4096 )
+#define appmainMQTT_OTA_UPDATE_TASK_PRIORITY      ( tskIDLE_PRIORITY )
+
+/**
+ * @brief Stack size and priority for MQTT agent task.
+ * Stack size is capped to an adequate value based on requirements from MbedTLS stack
+ * for establishing a TLS connection. Task priority of MQTT agent is set to a priority
+ * higher than other MQTT application tasks, so that the agent can drain the queue
+ * as work is being produced.
+ */
+#define appmainMQTT_AGENT_TASK_STACK_SIZE         ( 6144 )
+#define appmainMQTT_AGENT_TASK_PRIORITY           ( tskIDLE_PRIORITY + 2 )
+
+/**
+ * @brief Stack size and priority for CLI task.
+ */
+#define appmainCLI_TASK_STACK_SIZE                ( 6144 )
+#define appmainCLI_TASK_PRIORITY                  ( tskIDLE_PRIORITY + 1 )
+
+
+
+extern void vOTAUpdateTask( void * pvParam );
+
+
+
+#define mainLOGGING_TASK_STACK_SIZE         ( configMINIMAL_STACK_SIZE * 6 )
+#define mainLOGGING_MESSAGE_QUEUE_LENGTH    ( 15 )
+#define mainTEST_RUNNER_TASK_STACK_SIZE    ( configMINIMAL_STACK_SIZE * 8 )
+#define UNSIGNED_SHORT_RANDOM_NUMBER_MASK         (0xFFFFUL)
+/* The MAC address array is not declared const as the MAC address will
+normally be read from an EEPROM and not hard coded (in real deployed
+applications).*/
+static uint8_t ucMACAddress[ 6 ] =
+{
+    configMAC_ADDR0,
+    configMAC_ADDR1,
+    configMAC_ADDR2,
+    configMAC_ADDR3,
+    configMAC_ADDR4,
+    configMAC_ADDR5
+}; //XXX
+
+/* Define the network addressing.  These parameters will be used if either
+ipconfigUDE_DHCP is 0 or if ipconfigUSE_DHCP is 1 but DHCP auto configuration
+failed. */
+static const uint8_t ucIPAddress[ 4 ] =
+{
+    configIP_ADDR0,
+    configIP_ADDR1,
+    configIP_ADDR2,
+    configIP_ADDR3
+};
+static const uint8_t ucNetMask[ 4 ] =
+{
+    configNET_MASK0,
+    configNET_MASK1,
+    configNET_MASK2,
+    configNET_MASK3
+};
+static const uint8_t ucGatewayAddress[ 4 ] =
+{
+    configGATEWAY_ADDR0,
+    configGATEWAY_ADDR1,
+    configGATEWAY_ADDR2,
+    configGATEWAY_ADDR3
+};
+
+/* The following is the address of an OpenDNS server. */
+static const uint8_t ucDNSServerAddress[ 4 ] =
+{
+    configDNS_SERVER_ADDR0,
+    configDNS_SERVER_ADDR1,
+    configDNS_SERVER_ADDR2,
+    configDNS_SERVER_ADDR3
+};
+
+/**
+ * @brief Application task startup hook.
+ */
+void vApplicationDaemonTaskStartupHook( void );
+
+/**
+ * @brief Initializes the board.
+ */
+static void prvMiscInitialization( void );
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief The application entry point from a power on reset is PowerON_Reset_PC()
+ * in resetprg.c.
+ */
+void main( void )
+{
+    while(1)
+    {
+        vTaskDelay(10000);
+    }
+}
+/*-----------------------------------------------------------*/
+
+static void prvMiscInitialization( void )
+{
+    /* Initialize UART for serial terminal. */
+    uart_config();
+
+    UserInitialization();
+    /* Start logging task. */
+    xLoggingTaskInitialize( mainLOGGING_TASK_STACK_SIZE,
+                            tskIDLE_PRIORITY,
+                            mainLOGGING_MESSAGE_QUEUE_LENGTH );
+    FreeRTOS_printf( ( "Initialized UART\n" ) );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationDaemonTaskStartupHook( void )
+{
+	prvMiscInitialization();
+
+
+    {
+        /* Initialise the RTOS's TCP/IP stack.  The tasks that use the network
+        are created in the vApplicationIPNetworkEventHook() hook function
+        below.  The hook function is called when the network connects. */
+
+
+    	FreeRTOS_IPInit( ucIPAddress,
+                         ucNetMask,
+                         ucGatewayAddress,
+                         ucDNSServerAddress,
+                         ucMACAddress );
+
+        /* We should wait for the network to be up before we run any demos. */
+        while( FreeRTOS_IsNetworkUp() == pdFALSE )
+        {
+            vTaskDelay(300);
+        }
+
+        FreeRTOS_printf( ( "Initialise the RTOS's TCP/IP stack\n" ) );
+
+        /* Provision the device with AWS certificate and private key. */
+        vDevModeKeyProvisioning();
+
+        FreeRTOS_printf( ( "---------STARTING DEMO---------\r\n" ) );
+
+        /* Run all demos. */
+//      vStartSimplePubSubDemo();
+		vStartPKCSMutualAuthDemo();
+//      vStartOtaDemo();
+    }
+}
