e495356eb56969a4c34712d2001bc8fbab497770
RESG_XX <xiao-xuan.ngew.xc@renesas.com>
2023-06-23 11:03:32 +0800

    [Update] Updated XML to make compatible with new structure
    Signed-off-by: RESG_XX <xiao-xuan.ngew.xc@renesas.com>

--- Configuration/samples/bootloader/ck-rx65n/src/smc_gen/r_pincfg/Pin.c
+++ Configuration/samples/bootloader/ck-rx65n/src/smc_gen/r_pincfg/Pin.c
@@ -14,45 +14,56 @@
 * following link:
 * http://www.renesas.com/disclaimer
 *
-* Copyright (C) 2023 Renesas Electronics Corporation. All rights reserved.
+* Copyright (C) 2018 Renesas Electronics Corporation. All rights reserved.
 ***********************************************************************************************************************/
+
 /***********************************************************************************************************************
-* File Name    : r_s12ad_rx_pinset.c
-* Version      : 1.0.2
-* Device(s)    : R5F565NEHxFB
-* Tool-Chain   : RXC toolchain
-* Description  : Setting of port and mpc registers
+* File Name        : Pin.c
+* Version          : 1.0.2
+* Device(s)        : R5F565NEHxFB
+* Description      : This file implements SMC pin code generation.
 ***********************************************************************************************************************/
+
+/***********************************************************************************************************************
+Pragma directive
+***********************************************************************************************************************/
+/* Start user code for pragma. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
 
 /***********************************************************************************************************************
 Includes
 ***********************************************************************************************************************/
-#include "r_s12ad_rx_pinset.h"
-#include "platform.h"
+#include "r_cg_macrodriver.h"
+/* Start user code for include. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
+#include "r_cg_userdefine.h"
 
 /***********************************************************************************************************************
 Global variables and functions
 ***********************************************************************************************************************/
+/* Start user code for global. Do not edit comment generated here */
+/* End user code. Do not edit comment generated here */
 
 /***********************************************************************************************************************
-* Function Name: R_ADC_PinSet_S12AD0
-* Description  : This function initializes pins for r_s12ad_rx module
-* Arguments    : none
-* Return Value : none
+* Function Name: R_Pins_Create
+* Description  : This function initializes Smart Configurator pins
+* Arguments    : None
+* Return Value : None
 ***********************************************************************************************************************/
-void R_ADC_PinSet_S12AD0()
+
+void R_Pins_Create(void)
 {
     R_BSP_RegisterProtectDisable(BSP_REG_PROTECT_MPC);
 
-    /* Set AN000 pin */
-    PORT4.PCR.BIT.B0 = 0U;
-    PORT4.PDR.BIT.B0 = 0U;
-    PORT4.PMR.BIT.B0 = 0U;
-    MPC.P40PFS.BYTE = 0x80U;
+    /* Set RXD5 pin */
+    MPC.PC2PFS.BYTE = 0x0AU;
+    PORTC.PMR.BYTE |= 0x04U;
 
-    /* Set ADTRG0# pin */
-    MPC.P07PFS.BYTE = 0x09U;
-    PORT0.PMR.BIT.B7 = 1U;
+    /* Set TXD5 pin */
+    PORTC.PODR.BYTE |= 0x08U;
+    MPC.PC3PFS.BYTE = 0x0AU;
+    PORTC.PDR.BYTE |= 0x08U;
+    // PORTC.PMR.BIT.B3 = 1U; // Please set the PMR bit after TE bit is set to 1.
 
     R_BSP_RegisterProtectEnable(BSP_REG_PROTECT_MPC);
 }
