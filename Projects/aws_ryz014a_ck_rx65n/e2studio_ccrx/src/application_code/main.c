39ab537c6667661a13d084bf8ce391103b047f08
RESG_XX <xiao-xuan.ngew.xc@renesas.com>
2023-06-25 23:56:11 +0800

    [Update] Update "pcApplicationHostnameHook" function to return thingname written by CLI/LittleFS
    Signed-off-by: RESG_XX <xiao-xuan.ngew.xc@renesas.com>

--- Projects/aws_ryz014a_ck_rx65n/e2studio_ccrx/src/application_code/main.c
+++ Projects/aws_ryz014a_ck_rx65n/e2studio_ccrx/src/application_code/main.c
@@ -337,14 +337,23 @@ void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
 /*-----------------------------------------------------------*/
 
 #if ( ipconfigUSE_LLMNR != 0 ) || ( ipconfigUSE_NBNS != 0 ) || ( ipconfigDHCP_REGISTER_HOSTNAME == 1 )
-
+    /* This function will be called during the DHCP: the machine will be registered
+     * with an IP address plus this name. */
     const char * pcApplicationHostnameHook( void )
     {
-        /* This function will be called during the DHCP: the machine will be registered
-         * with an IP address plus this name. */
+#if defined(__TEST__)
         return clientcredentialIOT_THING_NAME;
+#else
+        if (gKeyValueStore.table[KVS_CORE_THING_NAME].valueLength > 0)
+        {
+            return GetStringValue (KVS_CORE_THING_NAME, gKeyValueStore.table[KVS_CORE_THING_NAME].valueLength + 1);
+        }
+        else
+        {
+            return clientcredentialIOT_THING_NAME;
+        }
+#endif
     }
-
 #endif
 
 bool ApplicationCounter(uint32_t xWaitTime)
