8aeeeb6a714d24acdd7a114d576f70b11a43b218
RESG_XX <xiao-xuan.ngew.xc@renesas.com>
2023-06-26 09:30:04 +0800

    [Update] Update FreeRTOS startup code to improve UX - Improved Processing_Before_Start_Kernel() routine
    Signed-off-by: RESG_XX <xiao-xuan.ngew.xc@renesas.com>

--- Projects/test_aws_ether_ck_rx65n/e2studio_ccrx/src/frtos_startup/freertos_start.c
+++ Projects/test_aws_ether_ck_rx65n/e2studio_ccrx/src/frtos_startup/freertos_start.c
@@ -343,9 +343,21 @@ void vApplicationTickHook(void)
 void Processing_Before_Start_Kernel(void)
 {
     BaseType_t ret;
+
+#if (RTOS_USB_SUPPORT == 1)
+	usb_rtos_err_t err = usb_rtos_configuration();
+	if (UsbRtos_Success != err)
+	{
+		while(1)
+		{
+			/** Failure of UsbRtos Configuration */
+		}
+	}
+#endif
+
     /************** task creation ****************************/
     /* Main task. */
-    ret = xTaskCreate(main, "MAIN_TASK", 512, NULL, 1, NULL);
+    ret = xTaskCreate(main_task, "MAIN_TASK", 512, NULL, 1, NULL);
     if (pdPASS != ret)
     {
         while(1)
