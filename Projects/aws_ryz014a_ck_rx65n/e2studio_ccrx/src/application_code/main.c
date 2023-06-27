406944ad2dd8878f9bf1e598ee78e9c26ad3b772
RESG_XX <xiao-xuan.ngew.xc@renesas.com>
2023-06-27 15:43:00 +0800

    [Update] Fix build error in RYZ014A project, caused by missing declaration
    Signed-off-by: RESG_XX <xiao-xuan.ngew.xc@renesas.com>

--- Projects/aws_ryz014a_ck_rx65n/e2studio_ccrx/src/application_code/main.c
+++ Projects/aws_ryz014a_ck_rx65n/e2studio_ccrx/src/application_code/main.c
@@ -51,7 +51,7 @@ static bool _wifiConnectAccessPoint( void );
 extern int32_t littlFs_init(void);
 bool ApplicationCounter(uint32_t xWaitTime);
 signed char vISR_Routine( void );
-
+extern KeyValueStore_t gKeyValueStore;
 extern void vStartSimplePubSubDemo( void  );
 
 #if (ENABLE_OTA_UPDATE_DEMO == 1)
