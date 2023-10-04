83ab27a4403d0a83bdf3c25a95dd7516019f62f9
KeitaKashima <keita.kashima.sx@renesas.com>
2023-10-04 15:08:58 +0900

    Added the check script of FIT module and version on CI/CD.

---
+++ Tools/module_check/expect_module_lists.py
@@ -0,0 +1,13 @@
+expected_values = [
+    {"display": "FreeRTOS_Kernel", "version": "1.0.120"},
+    {"display": "FreeRTOS_Object", "version": "1.0.120"},
+    {"display": "r_bsp", "version": "7.20"},
+    {"display": "r_byteq", "version": "2.00"},
+    {"display": "r_irq_rx", "version": "4.00"},
+    {"display": "r_s12ad_rx", "version": "5.00"},
+    {"display": "r_sci_rx", "version": "4.40"},
+    {"display": "r_tsip_rx", "version": "1.17.l"},
+    {"display": "r_fwup", "version": "2.00"},
+    {"display": "r_cellular", "version": "1.11"},
+    {"display": "r_flash_rx", "version": "5.00"},
+]
