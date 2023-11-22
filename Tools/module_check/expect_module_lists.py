a9ffb278995af615a1de6e624a2e82a0a7d5bef0
Dinh Van Nam <vannam.dinh.xt@renesas.com>
2023-11-22 17:13:44 +0900

    Fix check module version
    Signed-off-by: Dinh Van Nam <vannam.dinh.xt@renesas.com>

--- Tools/module_check/expect_module_lists.py
+++ Tools/module_check/expect_module_lists.py
@@ -8,7 +8,7 @@ expected_values = [
     {"display": "r_s12ad_rx", "version": "5.00"},
     {"display": "r_sci_rx", "version": "4.40"},
     {"display": "r_tsip_rx", "version": "1.17.l"},
-    {"display": "r_fwup", "version": "2.00"},
+    {"display": "r_fwup", "version": "2.01"},
     {"display": "r_cellular", "version": "1.11"},
     {"display": "r_flash_rx", "version": "5.00"},
 ]
